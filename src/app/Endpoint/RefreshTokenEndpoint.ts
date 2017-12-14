3d8439c5bf823e5a5a2755c942baf0645eb84d05
Justin <justin@homeceuconnection.com>
2017-12-14 08:34:39 -0600

    Updating module

--- src/app/Endpoint/RefreshTokenEndpoint.ts
+++ src/app/Endpoint/RefreshTokenEndpoint.ts
@@ -2,12 +2,13 @@ import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Rx";
 import {AsynchronousDefinition, Name, ObjectValidator, StringValidator} from "@ng-app-framework/validation";
 import {Endpoint, EndpointCaller, EndpointValidator} from "@ng-app-framework/api";
+import {OAuthConfig} from "../";
 
 @Name('RefreshTokenEndpoint')
 @Injectable()
 export class RefreshTokenEndpoint extends Endpoint {
 
-    public path: string = 'security/refresh';
+    public path: string = 'api/oauth/v2/token';
 
     validator: EndpointValidator = new EndpointValidator(
         new AsynchronousDefinition(new ObjectValidator('Refresh Token Request', {
@@ -20,12 +21,17 @@ export class RefreshTokenEndpoint extends Endpoint {
         }))
     );
 
-    constructor(public endpointCaller: EndpointCaller) {
+    constructor(public endpointCaller: EndpointCaller, public config: OAuthConfig) {
         super(endpointCaller);
     }
 
     public refresh(refreshToken: string): Observable<any> {
-        return this.request('get', {refresh_token: refreshToken});
+        return this.request('get', {
+            client_id: this.config.clientId,
+            client_secret: this.config.clientSecret,
+            grant_type: 'refresh_token',
+            refresh_token: refreshToken
+        });
     }
 
     public transformResponse(response: { data: any, message: string }) {
